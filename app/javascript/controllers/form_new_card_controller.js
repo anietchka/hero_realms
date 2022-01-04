d58ce0e9bc5b5f405a0cbb4504b2adfb925d165b
anna bozio <anna.bozio@gmail.com>
2022-01-04 13:51:33 +0100

    finish form for object card

--- app/javascript/controllers/form_new_card_controller.js
+++ app/javascript/controllers/form_new_card_controller.js
@@ -1,16 +1,23 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["firstQuestion", "collection", "secondQuestionIfObject"]
+  static targets = ["firstQuestion", "collection", "secondQuestionIfObject", "noColor", "sacrifiable", "optionsIfSacrifiable"]
 
-  goTo2() {
-    this.firstQuestionTarget.style.display = "none"
+  validateCollection(){
     if (this.collectionTarget.value === 'Objet') {
+      this.secondQuestionIfObjectTarget.classList.remove('d-none');
+      this.noColorTarget.value = 'Aucune'
+      console.log(this.noColorTarget.value)
       console.log("C'est une carte Objet");
-      this.secondQuestionIfObjectTarget.style.display = 'block'
-    } else if (this.collectionTarget.value === 'Action'){
+    } else if (this.collectionTarget.value === 'Action') {
       console.log("C'est une carte Action");
-    } else { console.log("C'est une carte Champion")}
-  }
+    } else { console.log("C'est une carte Champion") }
+  };
 
+  validateSacrifiable(){
+    if (this.sacrifiableTarget.checked === true){
+      this.optionsIfSacrifiableTarget.classList.remove('d-none')
+    } else { this.optionsIfSacrifiableTarget.classList.add('d-none')}
+    console.log(this.sacrifiableTarget.checked)
+  }
 }
