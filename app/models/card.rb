35282390acc7509cecb9cdf4b1aadfae7c65989b
anna bozio <anna.bozio@gmail.com>
2022-01-05 23:01:24 +0100

    remove active storage for card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,7 @@
 class Card < ApplicationRecord
   TYPE = %w[Objet Action Champion]
   COLOR = %w[Aucune Verte Rouge Bleue Jaune]
-  has_one_attached :photo
+
   validates :name, presence: true
   validates :collection, inclusion: { in: TYPE }
   validates :color, inclusion: { in: COLOR}
