54f719ca47441fe04bdaa75c582f71187ed69efb
anna bozio <anna.bozio@gmail.com>
2022-01-07 15:40:06 +0100

    add position model'

--- app/models/playground.rb
+++ app/models/playground.rb
@@ -1,3 +1,4 @@
 class Playground < ApplicationRecord
   has_many :players
+  has_many :positions, through: :players
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_140325) do
+ActiveRecord::Schema.define(version: 2022_01_07_143729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,14 @@ ActiveRecord::Schema.define(version: 2022_01_07_140325) do
     t.string "name"
   end
 
+  create_table "positions", force: :cascade do |t|
+    t.string "name"
+    t.bigint "player_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["player_id"], name: "index_positions_on_player_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -130,4 +138,5 @@ ActiveRecord::Schema.define(version: 2022_01_07_140325) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cards", "users"
   add_foreign_key "players", "playgrounds"
+  add_foreign_key "positions", "players"
 end
