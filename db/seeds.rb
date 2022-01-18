20968199049c401c3cf632655df47527ffa1230a
anna bozio <anna.bozio@gmail.com>
2022-01-18 16:05:27 +0100

    add deck player 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -149,9 +149,45 @@ Card.create!(
 )
 end
 puts 'Deck de base joueur 3 créé'
+puts 'Création deck de base joueur 4'
+Card.create!(
+  name: "Dague",
+  collection: "Objet",
+  color: "Aucune",
+  damage: 1,
+  photo: "Dague.jpg",
+  position: Position.find_by(name: "Pioche_4")
+)
+Card.create!(
+  name: "Epée courte",
+  collection: "Objet",
+  color: "Aucune",
+  damage: 2,
+  photo: "Epee_courte.jpg",
+  position: Position.find_by(name: "Pioche_4")
+)
+Card.create!(
+  name: "Rubis",
+  collection: "Objet",
+  color: "Aucune",
+  cash: 2,
+  photo: "Rubis.jpg",
+  position: Position.find_by(name: "Pioche_4")
+)
+7.times do
+  Card.create!(
+  name: "Or",
+  collection: "Objet",
+  color: "Aucune",
+  cash: 1,
+  photo: "Or.jpg",
+  position: Position.find_by(name: "Pioche_4")
+)
+end
+puts 'Deck de base joueur 4 créé!'
 
 puts 'Creating a lot of Gemme de Feu'
-2.times do
+16.times do
   Card.create!(
     name: 'Gemme de Feu',
     collection: 'Objet',
