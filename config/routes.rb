c9e87a08402b817cc490cb9651994ad93b579fa5
anna bozio <anna.bozio@gmail.com>
2022-01-18 17:14:18 +0100

    add index to playground'

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :cards
-  resources :playgrounds, only: :show
+  resources :playgrounds, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
