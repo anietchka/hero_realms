e241624b6a033e94d03f5ccc2d23c6e394a136c6
anna bozio <anna.bozio@gmail.com>
2022-01-18 17:14:57 +0100

    change playground and card controller

--- app/controllers/playgrounds_controller.rb
+++ app/controllers/playgrounds_controller.rb
@@ -1,6 +1,14 @@
 class PlaygroundsController < ApplicationController
-
+  def index
+    @playgrounds = Playground.all
+  end
   def show
     @playground = Playground.find(params[:id])
+    @cards_pioche = Card.all.where(position: Position.find_by(name: "Pioche"))
+    @pioche = Player.find_by(name: "Market")
+    @player1 = Player.find_by(name: "Player_1")
+    @player2 = Player.find_by(name: "Player_2")
+    @player3 = Player.find_by(name: "Player_3")
+    @player4 = Player.find_by(name: "Player_4")
   end
 end
